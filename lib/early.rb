7d24e271ae84800cf1c726a0d8f76004500ce09c
Genadi Samokovarov <gsamokovarov@gmail.com>
2018-02-21 20:12:07 +0200

    Release 0.2.0

--- lib/early.rb
+++ lib/early.rb
@@ -7,7 +7,7 @@
 # piece of code is hit, which may happen late in the program runtime an be easy
 # to miss.
 module Early
-  VERSION = '0.1.1'
+  VERSION = '0.2.0'
 
   class Configuration # :nodoc:
     attr_reader :variables
